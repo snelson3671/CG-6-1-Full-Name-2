8ec9cd46bb223a7c4efbfaa635f3a3e625dcd181
Sandra <snelson3671@yahoo.com>
2018-08-18 22:10:24 -0500

    Add project files.

---
+++ CG 6-1 Full Name 2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CG_6_1_Full_Name_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
